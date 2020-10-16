26859b23c8dcca239c50b04dc99b253926222abc
Faruk Ahmed <ahmed217@users.noreply.github.com>
2020-10-15 21:04:05 -0500

    Save schedule is working...

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ database = "sqlite:///controlpanel.db"
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
@@ -32,57 +33,72 @@ def index():
     instructor = Instructor()
     timeslot = TimeSlot()
     
+    schedules = Schedule.query.all()
+    
     return render_template("index.html", rooms = room.getRooms(), courses = course.getCourses(), 
-                           instructors = instructor.getInstructors(), timeslots = timeslot.getTimeSlots(),title='Control Panel')
+                           instructors = instructor.getInstructors(), timeslots = timeslot.getTimeSlots(),
+                           schedules = schedules, title='Control Panel')
     
-@app.route('/add/<item>/<id>')
-def additem(item, id):
-    
-    room_text = [-1,'']
-    instruct_text = [-1,'']
-    course_text = [-1,'']
-    timeslot_text = [-1,'']
-    
-    room = Room()
-    course = Course()
-    instructor = Instructor()
-    timeslot = TimeSlot()
-    
-    int_id = int(id)
-    
-    if re.match(r'rooms',item):
-        room_text = room.getRoomById(int_id)
-        
-    if re.match(r'instructors',item):
-        instruct_text = instructor.getInstructorById(int_id)
-        
-    if re.match(r'courses',item):
-        course_text = course.getCourseById(int_id)
-        
-    if re.match(r'timeslots',item):
-        timeslot_text = timeslot.getTimeSlotById(int_id)
-    
-    
-    return render_template("index.html", room_text=room_text[1], instruct_text=instruct_text[1],
-                           course_text=course_text[1],timeslot_text=timeslot_text[1],
-                           rooms = room.getRooms(), courses = course.getCourses(), 
-                           instructors = instructor.getInstructors(), timeslots = timeslot.getTimeSlots(),title='Control Panel')
-    
-
-# @app.route('/create', methods=['GET','POST'])
-# def create():
-#     if request.form:
-#         brand = request.form.get("brand")
-#         price = request.form.get("price")
-#         brokenlaptop = BrokenLaptop(brand=brand,price=price)
-#         db.session.add(brokenlaptop)
-#         db.session.commit()
-#         return redirect("/")   
+# @app.route('/add/<item>/<id>')
+# def additem(item, id):
 #     
-#     #brokenlaptops = BrokenLaptop.query.all()
-#     #return render_template("create.html",brokenlaptops=brokenlaptops)
-#     return render_template("create.html")
+#     room_text = [-1,'']
+#     instruct_text = [-1,'']
+#     course_text = [-1,'']
+#     timeslot_text = [-1,'']
+#     
+#     room = Room()
+#     course = Course()
+#     instructor = Instructor()
+#     timeslot = TimeSlot()
+#     
+#     int_id = int(id)
+#     
+#     if re.match(r'rooms',item):
+#         room_text = room.getRoomById(int_id)
+#         
+#     if re.match(r'instructors',item):
+#         instruct_text = instructor.getInstructorById(int_id)
+#         
+#     if re.match(r'courses',item):
+#         course_text = course.getCourseById(int_id)
+#         
+#     if re.match(r'timeslots',item):
+#         timeslot_text = timeslot.getTimeSlotById(int_id)
+#     
+#     
+#     return render_template("index.html", room_text=room_text[1], instruct_text=instruct_text[1],
+#                            course_text=course_text[1],timeslot_text=timeslot_text[1],
+#                            rooms = room.getRooms(), courses = course.getCourses(), 
+#                            instructors = instructor.getInstructors(), timeslots = timeslot.getTimeSlots(),title='Control Panel')
+#     
 
+@app.route('/create', methods=['GET','POST'])
+def create():
+    if request.form:
+        room_id = request.form.get("room_id")
+        room_name = request.form.get("room_name")
+        
+        instruct_id = request.form.get("instruct_id")
+        instruct_name = request.form.get("instruct_name")
+         
+        course_id = request.form.get("course_id")
+        course_name = request.form.get("course_name")
+         
+        timeslot_id = request.form.get("timeslot_id")
+        timeslot_name = request.form.get("timeslot_name")
+        #return request.form
+     
+        schedule = Schedule(room_id = room_id, room_name = room_name, instruct_id = instruct_id, instruct_name = instruct_name,
+                             course_id=course_id, course_name=course_name, timeslot_id = timeslot_id, timeslot_name = timeslot_name)
+         
+        db.session.add(schedule)
+        db.session.commit()
+        return redirect("/")   
+    
+@app.route('/init_db')
+def init_db():
+    db.create_all()
 
 # @app.route('/delete/<laptop_id>') # add id
 # def delete(laptop_id):
@@ -111,12 +127,16 @@ def additem(item, id):
 #     brokenlaptop = BrokenLaptop.query.filter_by(id=laptop_id).first()
 #     return render_template('update.html', brokenlaptop = brokenlaptop)
 # 
-# class Schedule(db.Model):
-#     id = db.Column(db.Integer, primary_key = True)
-#     room_id = db.Column(db.Integer, nullable = True)
-#     instruct_id = db.Column(db.Integer, nullable = True)
-#     course_id = db.Column(db.Integer, nullable = True)
-#     timeslot_id = db.Column(db.Integer, nullable = True)
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    room_id = db.Column(db.Integer, nullable = True)
+    room_name = db.Column(db.String(100), nullable = True)
+    instruct_id = db.Column(db.Integer, nullable = True)
+    instruct_name = db.Column(db.String(100), nullable = True)
+    course_id = db.Column(db.Integer, nullable = True)
+    course_name = db.Column(db.String(100), nullable = True)
+    timeslot_id = db.Column(db.Integer, nullable = True)
+    timeslot_name = db.Column(db.String(100), nullable = True)
 
 
 
@@ -143,7 +163,7 @@ class Instructor():
 
 class TimeSlot():
     def __init__(self):
-        self.timeslots = [[0,"M,W,F 9:00 - 10:00"],[1,"M,W,F 11:00 - 12:00"],[2,"TR 10:00 - 11:00"]]
+        self.timeslots = [[0,"MWF 9:00 - 10:00"],[1,"MWF 11:00 - 12:00"],[2,"TR 10:00 - 11:00"]]
     
     def getTimeSlotById(self, index):
         return self.timeslots[index]
